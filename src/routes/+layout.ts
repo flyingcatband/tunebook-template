b89d107b11e2b20899d0d5d75a2b4d6afdbae211
Caro <caro401@users.noreply.github.com>
2023-10-30 09:49:44 +0000

    Prerender all pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LoadEvent } from '@sveltejs/kit';
 import type { Folder } from '@flyingcatband/tunebook/types';
 
+export const prerender = true;
+
 // This is where the site gets the data for your tunes and sets
 export function load({ fetch }: LoadEvent): Promise<{ folder: Folder }> {
 	return fetch('/folder.json')
