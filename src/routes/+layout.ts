c3759ab3b2a16ff663e021eb56c53276eef2fdb0
James Rhodes <jarhodes314@gmail.com>
2025-03-11 23:51:42 +0000

    Update to 0.7.1 with the improved inference

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,10 @@
 import type { LoadEvent } from '@sveltejs/kit';
-import type { Folder, SetWithNextPrev } from '@flyingcatband/tunebook/types';
+import type { MungedFolder } from './folder.json/+server.js';
 
 export const prerender = true;
 
 // This is where the site gets the data for your tunes and sets
-export function load({ fetch }: LoadEvent): Promise<{ folder: Folder<SetWithNextPrev> }> {
+export function load({ fetch }: LoadEvent): Promise<{ folder: MungedFolder }> {
 	return fetch('/folder.json')
 		.then((res) => res.json())
 		.then((folder) => ({
--- src/routes/folder.json/+server.ts
+++ src/routes/folder.json/+server.ts
@@ -5,7 +5,14 @@ export const prerender = true;
 
 // This is where the site turns your abc and optionally tex files into a format it can work with
 export function GET(): Promise<Response> {
-	return generateFolderFromLatex('MyTunebook', './data/folder.tex')
-		.then(addNextPreviousSlugs)
-		.then(json);
+	return generateFolder().then(json);
+}
+
+export type MungedFolder = Awaited<ReturnType<typeof generateFolder>>;
+
+// Generates the data for folder.json.
+// Do everything in here apart from calling `json` to convert the result to a Response.
+// That way, `MungedFolder` can be used in `src/routes/+layout.ts` to type the data.
+function generateFolder() {
+	return generateFolderFromLatex('MyTunebook', './data/folder.tex').then(addNextPreviousSlugs);
 }
